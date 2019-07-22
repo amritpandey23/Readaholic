e81bff4c2c83e9b365223de43f5e284078469cf8
Amrit Pandey <amritpandey@tuta.io>
2019-07-22 22:34:21 +0530

    templates add; css included; routes added.

--- book_review/routes.py
+++ book_review/routes.py
@@ -1,6 +1,8 @@
 from book_review import app
+from flask import render_template, url_for
 
 @app.route("/")
+@app.route("/catalog")
 def home():
-    return "It works!"
+    return render_template("home.html")
 
