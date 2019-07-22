3e06b70b9d352f6a71d9dc94b2d25ffbe9739b54
Amrit Pandey <amritpandey@tuta.io>
2019-07-23 00:16:24 +0530

    Books model

---
+++ book_review/models.py
@@ -0,0 +1,12 @@
+from book_review import db
+
+class Books(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String(120), unique = True, nullable = False)
+    author = db.Column(db.String(80), nullable = False)
+    cover_image = db.Column(db.String(120), nullable = False, default = "default.jpeg")
+    short_description = db.Column(db.Text, nullable = False)
+    isbn = db.Column(db.Integer, nullable = True, default=1343)
+    review_text = db.Column(db.Text, nullable = False)
+    def __repr__(self):
+        return f"Name: {self.name}, Author: {self.author}"
