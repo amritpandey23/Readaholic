35bb2f50a54630364e09b63c741bb0cc0fe168c4
Amrit Pandey <amritpandey@tuta.io>
2019-07-23 00:18:29 +0530

    admin model

--- book_review/models.py
+++ book_review/models.py
@@ -10,3 +10,8 @@ class Books(db.Model):
     review_text = db.Column(db.Text, nullable = False)
     def __repr__(self):
         return f"Name: {self.name}, Author: {self.author}"
+
+class Admin(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String(20), nullable = False)
+    password = db.Column(db.String(100), nullable = False)
