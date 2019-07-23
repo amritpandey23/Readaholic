5de0a602cd083c1b1f5af1d6b7f6b03d9e57475e
Amrit Pandey <amritpandey@tuta.io>
2019-07-23 21:56:53 +0530

    change in column names

--- book_review/forms.py
+++ book_review/forms.py
@@ -1,17 +1,18 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, TextAreaField
+from wtforms import StringField, PasswordField, SubmitField, TextAreaField, SelectField
 from wtforms.validators import DataRequired, Length, Email
 
 class LoginForm(FlaskForm):
     email = StringField("Email", validators=[Email(), DataRequired()])
-    password = PasswordField("Password", validators=[DataRequired(), Length(min=6, message="Password must be 6 characters long")])
+    password = PasswordField("Password", validators=[DataRequired(), Length(min=6, message="Password should be 6 characters long")])
     submit = SubmitField("Login")
 
 class ReviewForm(FlaskForm):
-    name = StringField(label="Name", validators=[DataRequired(), Length(max=120)])
-    author = StringField(label="Author", validators=[DataRequired(), Length(max=40)])
-    cover_image = StringField(label="Cover Image", default="default.jpeg", validators=[DataRequired()])
-    short_description = TextAreaField(label="Short description of the book", validators=[DataRequired()])
+    book_title = StringField(label="Name", validators=[DataRequired(), Length(max=120)])
+    author_name = StringField(label="Author", validators=[DataRequired(), Length(max=40)])
+    cover_image_file = StringField(label="Cover Image", default="default.jpeg", validators=[DataRequired()])
     isbn = StringField(label="ISBN Number", default="1234", validators=[DataRequired()])
-    review_text = TextAreaField(label="Write Review", validators=[DataRequired()])
+    tag = SelectField(label="Choose Tag", choices=[("edu", "Education"), ("dev", "Development")])
+    tiny_summary = TextAreaField(label="Short description of the book", validators=[DataRequired()])
+    review_content = TextAreaField(label="Write Review", validators=[DataRequired()])
     submit = SubmitField(label="Add Review")
