fb70a0936e0939f2069e711dfa02396de0e346e0
Amrit Pandey <amritpandey@tuta.io>
2019-07-23 16:20:11 +0530

    removed name; used email

--- book_review/models.py
+++ book_review/models.py
@@ -13,5 +13,5 @@ class Books(db.Model):
 
 class Admin(db.Model):
     id = db.Column(db.Integer, primary_key = True)
-    name = db.Column(db.String(20), nullable = False)
+    email = db.Column(db.String(20), nullable = False)
     password = db.Column(db.String(100), nullable = False)
