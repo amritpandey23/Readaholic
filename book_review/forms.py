61a36296e963ea58ba59dea06a604584545a6871
Amrit Pandey <amritpandey@tuta.io>
2019-07-24 18:46:58 +0530

    admin form

--- book_review/forms.py
+++ book_review/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, TextAreaField, SelectField
-from wtforms.validators import DataRequired, Length, Email
+from wtforms import StringField, PasswordField, SubmitField, TextAreaField, SelectField, FileField
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from book_review.models import Admin
 
 class LoginForm(FlaskForm):
     email = StringField("Email", validators=[Email(), DataRequired()])
@@ -8,11 +9,22 @@ class LoginForm(FlaskForm):
     submit = SubmitField("Login")
 
 class ReviewForm(FlaskForm):
-    book_title = StringField(label="Name", validators=[DataRequired(), Length(max=120)])
+    book_title = StringField(label="Title", validators=[DataRequired(), Length(max=120)])
     author_name = StringField(label="Author", validators=[DataRequired(), Length(max=40)])
-    cover_image_file = StringField(label="Cover Image", default="default.jpeg", validators=[DataRequired()])
-    isbn = StringField(label="ISBN Number", default="1234", validators=[DataRequired()])
-    tag = SelectField(label="Choose Tag", choices=[("edu", "Education"), ("dev", "Development")])
-    tiny_summary = TextAreaField(label="Short description of the book", validators=[DataRequired()])
-    review_content = TextAreaField(label="Write Review", validators=[DataRequired()])
+    cover_image_file = FileField(label="Upload Cover Image")
+    isbn = StringField(label="ISBN", default="1234", validators=[DataRequired()])
+    tag = SelectField(label="Tag", choices=[("edu", "Education"), ("dev", "Development")])
+    tiny_summary = TextAreaField(label="Tiny Summary", validators=[DataRequired()])
+    review_content = TextAreaField(label="Review", validators=[DataRequired()])
     submit = SubmitField(label="Add Review")
+
+class AdminForm(FlaskForm):
+    email = StringField(label="Email", validators=[DataRequired()])
+    password = PasswordField(label="Password", validators=[DataRequired()])
+    confirm_password = PasswordField(label="Confirm Password", validators=[DataRequired(), EqualTo("password")])
+    submit = SubmitField("Register this user as admin")
+
+    def validate_email(self, email):
+        admin_user = Admin.query.filter_by(email = email.data).first()
+        if admin_user:
+            raise ValidationError("That email is already registered.", "warning")
