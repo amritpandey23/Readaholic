58be9658b2dcc11b63c9dacc03427388c103af40
Amrit Pandey <amritpandey@tuta.io>
2019-07-25 15:48:51 +0530

    more book tags; rating field

--- book_review/forms.py
+++ book_review/forms.py
@@ -3,6 +3,16 @@ from wtforms import StringField, PasswordField, SubmitField, TextAreaField, Sele
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from book_review.models import Admin
 
+book_tags = [
+    ("Null", "Choose..."),
+    ("edu", "Education"),
+    ("dev", "Development"),
+    ("growth", "Personal Growth"),
+    ("programming", "Programming"),
+    ("novel", "Novel"),
+    ("fiction", "Fiction")
+]
+
 class LoginForm(FlaskForm):
     email = StringField("Email", validators=[Email(), DataRequired()])
     password = PasswordField("Password", validators=[DataRequired(), Length(min=6, message="Password should be 6 characters long")])
@@ -13,9 +23,10 @@ class BookForm(FlaskForm):
     author_name = StringField(label="Author", validators=[DataRequired(), Length(max=40)])
     cover_image_file = FileField(label="Upload Cover Image")
     isbn = StringField(label="ISBN", default="1234", validators=[DataRequired()])
-    tag = SelectField(label="Tag", choices=[("edu", "Education"), ("dev", "Development")])
+    tag = SelectField(label="Tag", choices=book_tags)
+    initial_rating = StringField(label="Initial Rating(out of 5)")
     tiny_summary = TextAreaField(label="Tiny Summary", validators=[DataRequired()])
-    submit = SubmitField(label="Add Book")
+    submit = SubmitField(label="Save")
 
 class AdminForm(FlaskForm):
     email = StringField(label="Email", validators=[DataRequired()])
