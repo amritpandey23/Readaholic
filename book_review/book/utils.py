f60aa0d9844cc84360de0fa81e7852d1e5087ffe
Amrit Pandey <amritpandey@tuta.io>
2019-07-29 14:40:25 +0530

    utils methods to save and delete cover image files

--- book_review/book/utils.py
+++ book_review/book/utils.py
@@ -1,9 +1,12 @@
-import os
+import os, uuid
 from book_review import app
 from werkzeug.utils import secure_filename
 
 def save_cover_image(cover_image):
   f = cover_image.data
-  filename = secure_filename(f.filename)
+  filename = f"{str(uuid.uuid4())}.{f.filename.rsplit('.', 1)[1].lower()}"
   f.save(os.path.join(app.instance_path, "uploads", filename))
   return filename
+
+def delete_cover_image(filename):
+  os.remove(os.path.join(app.instance_path, "uploads", filename))
