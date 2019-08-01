1b2919957e2837acca12ac8798c63c94f97acc54
Amrit Pandey <amrit@pop-os.localdomain>
2019-08-01 22:27:57 +0530

    some comments; change in variable names

--- book_review/models.py
+++ book_review/models.py
@@ -7,7 +7,7 @@ from flask_login import UserMixin
 def load_user(user_id):
     return Admin.query.get(int(user_id))
 
-
+# Table: Admin
 class Admin(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(20), nullable=False)
@@ -16,34 +16,44 @@ class Admin(db.Model, UserMixin):
     def __repr__(self):
         return f"Admin(Email: {self.email})"
 
-
+# Table: Book
 class Book(db.Model):
+
     id = db.Column(db.Integer, primary_key=True)
+
+    # String fields
     book_title = db.Column(db.String(120), unique=True, nullable=False)
-    title_slug = db.Column(db.String(120), unique=True, nullable=False)
+    title_slug = db.Column(db.String(100), unique=True, nullable=False)
     author_name = db.Column(db.String(80), nullable=False)
-    isbn = db.Column(db.Integer, nullable=False)
-    genre = db.Column(db.String(30), nullable=True)
-    rating = db.Column(db.Integer, nullable=True)
     shop_link = db.Column(db.String(100), nullable=True)
-    cover_image_file = db.Column(db.String(120), nullable=False, default="default.jpeg")
-    date_edited = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    genre = db.Column(db.String(30), nullable=True)
+    cover_image_file = db.Column(db.String(50), nullable=False, default="default.jpeg")
+
+    # Text Fields
     tiny_summary = db.Column(db.Text, nullable=True)
     review_content = db.Column(db.Text, nullable=True)
     review_content_draft = db.Column(db.Text, nullable=True)
+
+    # Integer Fields
+    isbn = db.Column(db.Integer, nullable=False)
+    rating = db.Column(db.Integer, nullable=False)
+
+    # Others
+    date_edited = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
     comments = db.relationship("Comment", backref="book", lazy=True)
 
     def __repr__(self):
         return f"Book(Title: '{self.book_title}', Author: '{self.author_name}', Cover Image: '{self.cover_image_file}', ISBN Number: '{self.isbn}')"
 
-
+# Table: Comments
 class Comment(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), nullable=False)
     name = db.Column(db.String(120), nullable=False, default="Anonymous")
     comment_text = db.Column(db.Text, nullable=False)
     book_id = db.Column(db.Integer, db.ForeignKey("book.id"), nullable=False)
-    mod_verified = db.Column(db.Boolean, nullable=False, default=False)
+    verified = db.Column(db.Boolean, nullable=False, default=False)
+    date_added = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
 
     def __repr__(self):
         return f"Comment(email: '{self.email}', comment_text: '{self.comment_text}', book_id: '{self.book_id}', mod_verified: '{self.mod_verified}')"
